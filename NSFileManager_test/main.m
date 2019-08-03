bf9332f66d1fbb10e0ee4eaac5c2b523f778a099
Gregory John Casamento <greg.casamento@gmail.com>
2019-08-03 01:17:04 -0400

    Initial Commit

---
+++ NSFileManager_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSFileManager_test
+//
+//  Created by Gregory John Casamento on 8/3/19.
+//  Copyright © 2019 Gregory John Casamento. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
